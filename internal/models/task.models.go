282afaff3916f9ae0a78618409b03d01beb5a571
Bruno <psbrunosouza@gmail.com>
2023-03-05 18:39:32 -0300

    feat: update task and subtask done field to add default false

--- internal/models/task.models.go
+++ internal/models/task.models.go
@@ -4,7 +4,7 @@ type Task struct {
 	Default
 	Description string    `json:"description,omitempty" gorm:"not null;default:null"`
 	Name        string    `json:"name,omitempty" gorm:"not null;default:null;type:varchar(255)"`
-	Done        bool      `json:"done,omitempty" gorm:"default:null;"`
+	Done        bool      `json:"done,omitempty" gorm:"default:false;"`
 	Workspace   Workspace `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	WorkspaceID int       `json:"workspaceID,omitempty" gorm:"default:null;"`
 	Color       Color     `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
